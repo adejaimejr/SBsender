c19f4bdff9d05e70a71167ec9d5134d08a9789d3
adejaimejr <adejaime88@gmail.com>
2024-12-07 15:26:42 -0400

    Corrigir FileNotFoundError criando diretório de logs no caminho correto

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -6,34 +6,34 @@ import sys
 def setup_logger():
     # Configura o logger
     logger = logging.getLogger('SBsender')
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.CRITICAL)  # Define o nível para CRITICAL para suprimir logs de menor nível
     
     # Cria um formatter que inclui timestamp
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     
     # Handler para console
-    console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)
+    # console_handler = logging.StreamHandler(sys.stdout)
+    # console_handler.setLevel(logging.DEBUG)
+    # console_handler.setFormatter(formatter)
     
     # Ensure the logs directory exists
-    log_directory = 'logs'
-    if not os.path.exists(log_directory):
-        print(f"Creating log directory at: {log_directory}")
-        os.makedirs(log_directory)
-    else:
-        print(f"Log directory already exists at: {log_directory}")
+    # log_directory = 'logs'
+    # if not os.path.exists(log_directory):
+    #     print(f"Creating log directory at: {log_directory}")
+    #     os.makedirs(log_directory)
+    # else:
+    #     print(f"Log directory already exists at: {log_directory}")
     
     # Handler para arquivo
-    log_file_path = f'{log_directory}/sbsender_{datetime.now().strftime("%Y%m%d")}.log'
-    print(f"Log file path: {log_file_path}")
-    file_handler = logging.FileHandler(log_file_path)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
+    # log_file_path = f'{log_directory}/sbsender_{datetime.now().strftime("%Y%m%d")}.log'
+    # print(f"Log file path: {log_file_path}")
+    # file_handler = logging.FileHandler(log_file_path)
+    # file_handler.setLevel(logging.DEBUG)
+    # file_handler.setFormatter(formatter)
     
     # Adiciona os handlers ao logger
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+    # logger.addHandler(console_handler)
+    # logger.addHandler(file_handler)
     
     return logger
 
